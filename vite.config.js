7f01f60ee5267ae4ae2db733c6ecef43f1aefef2
YugoSamaKūhaku <58355557+YugoSamakuhaku@users.noreply.github.com>
2023-09-05 20:08:24 +0700

    laravel breeze & adminlte3 first

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, normalizePath, build } from "vite";
+import laravel from "laravel-vite-plugin";
+
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  root: resolve(__dirname, "./"),
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(
+            resolve(__dirname, "./node_modules/admin-lte/dist")
+          ),
+          dest: "../adminlte3",
+        },
+        {
+          src: normalizePath(
+            resolve(__dirname, "./node_modules/admin-lte/plugins")
+          ),
+          dest: "../adminlte3",
+        },
+      ],
+    }),
+  ],
 });
